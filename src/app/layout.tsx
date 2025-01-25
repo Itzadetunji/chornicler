344b407779524cb8b9b880e39b80ee89a14a478c
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2025-01-25 17:03:15 +0100

    Chore: Updated mongodb, updated title icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,39 @@
 import QueryClientConfig from "@/components/QueryClientConfig";
 import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Chronicler Interview",
+	description: "Chronicler Interview",
+	authors: [{ name: "Chronicler" }],
+	themeColor: "#ffffff",
+	viewport:
+		"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+	icons: {
+		apple: [
+			{
+				url: "https://media.contra.com/image/upload/c_fill,f_avif,h_64,q_auto:good,w_64/shjzpq4ljsfxgw3wil5l",
+				type: "image/png",
+				sizes: "180x180",
+				rel: "apple-touch-icon",
+			},
+		],
+		icon: [
+			{
+				url: "https://media.contra.com/image/upload/c_fill,f_avif,h_64,q_auto:good,w_64/shjzpq4ljsfxgw3wil5l",
+				type: "image/png",
+				sizes: "32x32",
+				rel: "icon",
+			},
+			{
+				url: "https://media.contra.com/image/upload/c_fill,f_avif,h_64,q_auto:good,w_64/shjzpq4ljsfxgw3wil5l",
+				type: "image/png",
+				sizes: "16x16",
+				rel: "icon",
+			},
+		],
+	},
+};
 
 export default function RootLayout({
 	children,
@@ -9,9 +43,6 @@ export default function RootLayout({
 	return (
 		<QueryClientConfig>
 			<html lang="en">
-				<head>
-					<title>Chronicler Interview</title>
-				</head>
 				<body className="antialiased">{children}</body>
 			</html>
 		</QueryClientConfig>
